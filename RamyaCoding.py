94cad7038226613e4f0771f251deb3ba55c9c6ef
ramya kandari <ramyakandari7760@gmail.com>
2024-10-11 11:33:56 -0500

    initial commit

---
+++ RamyaCoding.py
@@ -0,0 +1,77 @@
+a=100
+print(a)
+
+b=2
+print(a+b)
+
+a = 'aj'
+b= 'ay'
+
+print(a +b)
+
+num = 2 +7j
+print(type(num))
+
+x=192
+x= complex(x)
+print(x)
+print(complex(2,5))
+
+a=4
+b=5
+print(a*b)
+
+import math
+print(1/ math.pi * math.pi)
+
+num=2+ 5j
+num.real
+num.imag
+z=[10]*10
+print(z)
+print(math.sqrt(81))
+a=[1,2,4,56,32,56]
+print(a)
+a[:3]
+a[2:4]
+a[::-1]
+a[::2]
+a=list('hi mom')
+print(a)
+num=[1,2,3,4]
+one, * other = num
+print(one) 
+print(other)
+print(num)
+num.append(6)
+print(num)
+stg=[ 'red', 'bed', 'hi']
+num.extend(stg)
+print(num)
+num.insert(2,'why')
+print(num)
+num.remove('why')
+print(num)
+var=[5,7,2,6,4,9,7]
+var.sort()
+print(var)
+len(var)
+max(var)
+sum(var)/ len(var)
+city=('great', 'hwe', 'yoy')
+print(city)
+print(type(city))
+num= (1,2,3,4)
+print(city + num)
+a = (city, num)
+print(a)
+a=[1,2,3]
+b=[4,5,6]
+c=[a,b]
+print(c)
+a= ('python')*5
+print(a)
+num=(1,2,3,4)
+num[1:]
+num[1:3]
+tuple('hey')
